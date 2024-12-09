631821d1a7ef4ba569bbee82e12fef2fcf31d4dd
twocucao <twocucao@gmail.com>
2024-12-10 02:21:04 +0800

    add-fireworks

--- src/Root.tsx
+++ src/Root.tsx
@@ -35,7 +35,7 @@ export const RemotionRoot: React.FC = () => {
       <Composition
         id="Fireworks"
         component={Clip}
-        durationInFrames={300}
+        durationInFrames={30 * 30}
         fps={30}
         width={1920}
         height={1080}
